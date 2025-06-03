4447e288f29bb48aa5bd9cec71da52cfe4301110
23144085 <23144085@ed.tmu.ac.jp>
2025-06-03 14:44:40 +0900

    変更

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ const createGestureRecognizer = async () => {
     gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
         baseOptions: {
             modelAssetPath:
-                "./models/gesture_recognizer.task",
+                "./gesture_recognizer.task",
                 
             delegate: "GPU"
         },
